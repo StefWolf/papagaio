30b3d35bc379ec1325e36d35f37fce02de04404e
StefWolf Sensitivel Lotus Flower <61637759+StefWolf@users.noreply.github.com>
2020-10-04 21:33:17 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TrabalhandoDicionario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            string gravar;
+            string digitar; 
+
+            Dictionary<string, string> frase = new Dictionary<string, string>();
+
+            Console.WriteLine("--------[Aprender]----------");
+            while(n < 1){
+                Console.WriteLine("Usuário: ");
+                string esc = Console.ReadLine();
+
+                if(esc == "Aprender"){
+                    Console.WriteLine("Quando digitarem: ");
+                    gravar = Console.ReadLine();
+                    Console.WriteLine("Devo digitar: ");
+                    digitar = Console.ReadLine();
+
+                    frase.Add(gravar, digitar);
+                }
+                else{
+                    gravar = esc;
+                    Console.WriteLine("Programa: "+frase[gravar]);
+                }          
+            }
+             Console.WriteLine("Fim da sessão"); 
+        }
+    }
+}
